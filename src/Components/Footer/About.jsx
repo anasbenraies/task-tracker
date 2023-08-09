3583f56da03d359359c22e6966fb6afe8a82b374
Software Engineer <a.benraies19038@pi.tn>
2023-08-09 16:28:19 +0100

    "second commit"

--- src/Components/Footer/About.jsx
+++ src/Components/Footer/About.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>
+    <div style={{display:"flex",flexDirection:'column',marginLeft:"47%"}}>
       <h4>Version 1.0.0</h4>
       <a href='/'>Go back</a>
     </div>
